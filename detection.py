250beaa191aca31026d3e57f24320a676cf9f2cc
LParker885 <67603811+LParker885@users.noreply.github.com>
2022-01-03 14:47:09 -0700

    Update detection.py

--- detection.py
+++ detection.py
@@ -3,7 +3,7 @@ import jetson.utils
 camera = jetson.utils.videoSource("/dev/video0")
 display = jetson.utils.videoOutput("display://0")
 #net = jetson.inference.detectNet('ssd-mobilenet-v2',threshold = 0.3)
-net = jetson.inference.detectNet(argv=["--model=/home/eagle/Glassesgood/Model/ssd-mobilenet.onnx","--labels=/home/eagle/Glassesgood/Model/labels.txt","--input-blob=input_0","--output-cvg=scores","--output-bbox=boxes"],threshold=0.6)
+net = jetson.inference.detectNet(argv=["--model=Model/ssd-mobilenet.onnx","--labels=Model/labels.txt","--input-blob=input_0","--output-cvg=scores","--output-bbox=boxes"],threshold=0.6)
 
 while display.IsStreaming():
     img = camera.Capture()
